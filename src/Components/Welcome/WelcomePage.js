7d60fafd2c6560f42790c22248d0af59b71ffed9
Prashant Randive <prashant.randhive05@gmail.com>
2023-12-03 14:44:39 +0530

    initial files upload

--- src/Components/Welcome/WelcomePage.js
+++ src/Components/Welcome/WelcomePage.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import './WelcomePage.css'
 import { useNavigate } from 'react-router-dom';
+// import video from '../../Assets/alone-house-in-forest.960x540.mp4'
+
 
 const WelcomePage = () => {
 
@@ -22,16 +24,20 @@ const WelcomePage = () => {
     };
 
     return (
-    <>
-        <form onSubmit={nameHandler} className='form-container'>
-            <input type='text' placeholder='Enter here the Name' value={wisher} onChange={(e) => setWisher(e.target.value)} />
-            <button type='submit'>Submit</button>
-        </form>
-        <section className='error-container'>
-            <p>{emptyInput}</p>
-        </section>
-
-    </>
+        <> <div className='video-background'>
+            {/* <video autoPlay loop muted>
+                <source src={video} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video> */}
+            <form onSubmit={nameHandler} className='form-container'>
+                <input type='text' placeholder='Enter here the Name' value={wisher} onChange={(e) => setWisher(e.target.value)} />
+                <button type='submit'>Submit</button>
+            </form>
+            <section className='error-container'>
+                <p>{emptyInput}</p>
+            </section>
+        </div>
+        </>
     )
 }
 
